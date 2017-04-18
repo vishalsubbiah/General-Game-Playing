491accea871b00c5c4cbada931e671bd016b6713
Vishal Subbiah <subbiahvishal@gmail.com>
2017-04-18 15:28:07 -0700

    week3 player

---
+++ codebase/ggp-base/src/main/java/week3Player.java
@@ -0,0 +1,70 @@
+import java.util.List;
+
+import org.ggp.base.player.gamer.exception.GamePreviewException;
+import org.ggp.base.util.game.Game;
+import org.ggp.base.util.statemachine.MachineState;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.Role;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.cache.CachedStateMachine;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
+
+public class week3Player extends BaseDemBoisGamer {
+
+	@Override
+	public StateMachine getInitialStateMachine() {
+		// TODO Auto-generated method stub
+		return new CachedStateMachine(new ProverStateMachine());
+
+	}
+
+	@Override
+	public void stateMachineMetaGame(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Move stateMachineSelectMove(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		StateMachine machine = getStateMachine();
+		MachineState state= getCurrentState();
+		List<Role> roles = machine.getRoles();
+		Role role = getRole();
+		if(roles.size()==1){
+			return getCompulsiveDeliberationMove(role, state, timeout);
+		}
+		else{
+			return this.getAlphaBetaMove(role, state, timeout);
+		}
+	}
+
+	@Override
+	public void stateMachineStop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stateMachineAbort() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void preview(Game g, long timeout) throws GamePreviewException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "Week3Player";
+	}
+
+}
